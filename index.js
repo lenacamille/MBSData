fa2891ba6dcde8a6807cef15c01a32d150ff21c3
Lena Erickson <lenacerickson@gmail.com>
2018-07-02 23:33:20 -0500

    added database connection

--- index.js
+++ index.js
@@ -31,3 +31,15 @@ es = express()
   }
   res.send(result)
 })
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    res.render('pages/db', result);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
