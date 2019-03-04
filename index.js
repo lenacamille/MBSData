d8bb887a0ceebf495118efae141661ca7b8e9aff
Lena Erickson <lenacerickson@gmail.com>
2019-03-03 20:56:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ setInterval(() => server.getConnections(
 ), 5000);
 
 //es.use(express.static(path.join(__dirname, 'public')))
-es.engine('html', require('ejs').renderFile);
+// es.engine('html', require('ejs').renderFile);
+es.set('view engine', 'ejs');
 es.use(express.static(path.join(path.join(__dirname, 'ant'), 'build')))
 // es.use(express.static(path.join(__dirname,'build')))
 es.get('/', (req, res) => res.render('/index.html'))
