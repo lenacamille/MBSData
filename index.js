ffa026214928121a4d64eee5707e6f449783f651
Lena Erickson <lenacerickson@gmail.com>
2019-03-03 19:54:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const cool = require('cool-ascii-faces')
+// const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
